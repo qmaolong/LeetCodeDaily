37af16a0fd722f40065236f688cb20b22341a5fd
qmaolong <qmaolong@qq.com>
2021-06-07 14:47:47 +0800

    ud

--- 494.目标和.go
+++ 494.目标和.go
@@ -19,3 +19,19 @@ func findTargetSumWays(nums []int, target int) int {
 }
 
 // @lc code=end
+
+/* 常规解法
+时间复杂度：O（2^n）
+Your runtime beats 25.91 % of golang submissions
+Your memory usage beats 83.15 % of golang submissions (2.2 MB)
+-------------------------
+func findTargetSumWays(nums []int, target int) int {
+	if len(nums) == 0 {
+		if target == 0 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+	return findTargetSumWays(nums[1:], target-nums[0]) + findTargetSumWays(nums[1:], target+nums[0])
+} */
