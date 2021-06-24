6c370bc8bc932c7d3d701931187ae97be675285b
qmaolong <qmaolong@qq.com>
2021-06-24 11:06:17 +0800

    ud

--- 149.直线上最多的点数.go
+++ 149.直线上最多的点数.go
@@ -5,11 +5,13 @@ package main
  *
  * [149] 直线上最多的点数
  */
-//  Your runtime beats 29.47 % of golang submissions
+//  Your runtime beats 88.42 % of golang submissions
 //  Your memory usage beats 90.53 % of golang submissions (2.7 MB)
 
 // @lc code=start
 func maxPoints(points [][]int) int {
+
+	//判断3个点是否为一条直线
 	var ifLine = func(p1, p2, p3 []int) bool {
 		if (p1[0]-p2[0]) == 0 || (p2[0]-p3[0]) == 0 {
 			return (p2[0]-p3[0]) == (p1[0]-p2[0]) && p1[0] == p2[0] && p1[0] == p3[0]
@@ -32,16 +34,11 @@ func maxPoints(points [][]int) int {
 			}
 			tCount := 2
 			arr[i][j] = &tCount
-			for k := 0; k < len(points); k++ {
-				if k == i || k == j {
-					continue
-				}
+			for k := j + 1; k < len(points); k++ {
 				if ifLine(points[i], points[j], points[k]) {
 					tCount++
 					arr[i][k] = &tCount
-					arr[k][i] = &tCount
 					arr[j][k] = &tCount
-					arr[k][j] = &tCount
 				}
 			}
 		}
@@ -49,7 +46,7 @@ func maxPoints(points [][]int) int {
 
 	max := 1
 	for i := range arr {
-		for j := range arr[i] {
+		for j := i + 1; j < len(arr[i]); j++ {
 			if arr[i][j] != nil && *arr[i][j] > max {
 				max = *arr[i][j]
 			}
